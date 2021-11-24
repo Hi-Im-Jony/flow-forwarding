ee6e24882132af1f2358c14ad3d75dcc58233a96
Hi-Im-Jony <jccicai@yahoo.ie>
2021-11-24 22:05:31 +0000

    basic router implementation

--- src/Router/Router.java
+++ src/Router/Router.java
@@ -1,12 +1,66 @@
 package Router;
+import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.SocketException;
-import java.util.HashMap;
 
 public class Router {
-    private int id;
+    private static int id;
+    final static int MTU = 1500;
+    final static int FS_PORT = 51510;
+
+    static DatagramSocket socket;
 
     public static void main(String[] args) throws SocketException {
-        System.out.println("Hello from Router");
+        
+        socket = new DatagramSocket();
+        id = Integer.parseInt(args[0]);
+        System.out.println("Hello from Router "+id);
+        RouterThread thread = new RouterThread();
+        thread.start();
+    }
+
+    private static class RouterThread extends Thread{
+        @Override
+        public void run() {
+            RouterThread thread = new RouterThread();
+            thread.start();
+
+            while(true){
+                try {
+                    receive();
+
+                } 
+                catch (IOException e) {e.printStackTrace();}
+            }
+        }
+    }
+
+    public static byte[] receive() throws IOException{
+          
+        byte[] data= new byte[MTU];
+        DatagramPacket packet= new DatagramPacket(data, data.length);
+    
+        socket.receive(packet);
+
+        // extract data from packet
+        data= packet.getData();
+
+        System.out.println("Router received: \""+data+",\" from: "+packet.getAddress());
+        
+        InetAddress address= InetAddress.getLocalHost();
+        send(data, address, FS_PORT);
+        return data;
+    }
+
+    public static void send(byte[] data, InetAddress address, int dest) throws IOException{
+        
+        // InetAddress address= InetAddress.getLocalHost();   
+        int port= dest;                       
+    
+        // create packet addressed to destination
+        DatagramPacket packet= new DatagramPacket(data, data.length, address, port);
+        socket.send(packet);
     }
 }
